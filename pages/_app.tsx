fe25312820f413486d465983367223f2973e58b4
sodoku <philip.d.stewart@gmail.com>
2023-06-26 21:59:47 +0200

    add app manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
+import Head from 'next/head';
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#060606"/>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
